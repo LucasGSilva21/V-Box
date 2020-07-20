99b6786a6b918bf5bf800661a86f20c64cefdb5c
Lucas Gabriel <lucasgsilva2102@gmail.com>
2020-07-20 16:36:29 -0300

    criando class App

---
+++ src/app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+
+require("./database/index");
+
+class App {
+  constructor() {
+    this.server = express();
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    this.server.use(express.json());
+  }
+
+  routes() {
+    this.server.use(cors());
+  }
+}
+
+module.exports = new App().server;
